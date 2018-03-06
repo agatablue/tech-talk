fe5030e5c0031ab7d641443f4b89111c75b5aa20
agatablue <agatablue@gmail.com>
2018-03-06 15:28:00 +0100

    add components

--- app.jsx
+++ app.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+class App extends React.Component {
+    render() {
+        return <div>
+            <header className="main_header"> Strona główna </header>
+            <nav>
+                <ul>
+                    <li>Start</li>
+                    <li>O nas</li>
+                    <li>Galeria</li>
+                    <li>Kontakt</li>
+                </ul>
+            </nav>
+            <section>
+                <article>
+                    Lorem ipsum
+                </article>
+                <article>
+                    Lorem ipsum 2
+                </article>
+            </section>
+
+            <footer>
+             2018
+            </footer>
+        </div>
+    }
+}
+
+
+
+
 document.addEventListener('DOMContentLoaded',
   function(){
     ReactDOM.render(
-      <h1>Hello, World!</h1>,
+      <App />,
       document.getElementById('app')
     );
 });
